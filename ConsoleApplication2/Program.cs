f4cfaf665382bcc034695a12d75303a4bb3464ed
Can <canturker00@gmail.com>
2021-04-07 21:35:19 +0300

    Completed

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,12 +1,67 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace ConsoleApplication2
 {
-    internal class Program
+    internal class FibonacciLookup
     {
+        
+        private const int InitSize=50;
+        private static long[] _table;
         public static void Main(string[] args)
         {
-            Debug.Close();
+            _table=new long[InitSize];
+            
+            Console.WriteLine("{0}",Fibonacci(40));
+            Console.WriteLine("{0}",StandardFibonacci(40));
+            
         }
+
+        private static long Fibonacci(int a)
+        {
+            if (a < 2)
+            {
+                _table[a] = a;
+                return a;
+            }
+
+            if (a>=_table.Length)
+            {
+                    while (a > _table.Length - 1)
+                    {
+                        long[] newTable=new long[_table.Length*2];
+                        _table.CopyTo(newTable,0);
+                        _table = newTable;
+                    }
+            }
+            if (_table[a] != 0)
+            {
+                return _table[a];
+            }
+
+            long value = 0;
+            try
+            {
+                 value = checked(Fibonacci(a - 1) + Fibonacci(a - 2));
+            }
+            catch 
+            {
+                Console.WriteLine("The number is too big!");
+                return 0;
+            }
+            _table[a] = value;
+            return value;
+        }
+
+        private static long StandardFibonacci(int a)
+        {
+            if (a < 2)
+            {
+                return a;
+            }
+
+            return StandardFibonacci(a - 1) + StandardFibonacci(a - 2);
+        }
+        
     }
 }
